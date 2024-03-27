06915c754dd048a2402aa8bf18687f7653ccbd48
Fatec <faelpower3d>
2024-03-27 19:15:38 -0300

    arrumado e funcionando

--- spring/src/main/java/com/programacao/web/fatec/apifatec/ApiFatecApplication.java
+++ spring/src/main/java/com/programacao/web/fatec/apifatec/ApiFatecApplication.java
@@ -1,12 +1,16 @@
 package com.programacao.web.fatec.apifatec;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import com.programacao.web.fatec.apifatec.entitines.Cliente;
 
+import com.programacao.web.fatec.apifatec.entitines.Cliente;
 import com.programacao.web.fatec.apifatec.domain.cliente.ClienteRepository;
 
+
+@SpringBootApplication
 public class ApiFatecApplication {
 	
 	
@@ -16,16 +20,12 @@ public class ApiFatecApplication {
 			Cliente cliente = new Cliente();
 			cliente.setNome("Danilo");
 			cliente.setEmail("h2danilofatec@hotmail.com");
-			cliente.setEndereco("Rua xxx, 126");
+			cliente.setEndereco("Rua pedro, 126");
 			cliente.setRazaoSocial("Danilo");
 			
 			clienteRepository.save(cliente);
 		};
-	}
-
-	
-	
-	
+	}	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
