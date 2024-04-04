05aa0fe2386796410559bf2df6eb73ac32dd31ce
Fatec <faelpower3d>
2024-04-04 20:50:03 -0300

    atualizado

--- spring/src/main/java/com/programacao/web/fatec/apifatec/controllers/exercicios/ExerciciosController.java
+++ spring/src/main/java/com/programacao/web/fatec/apifatec/controllers/exercicios/ExerciciosController.java
@@ -1,14 +1,13 @@
 package com.programacao.web.fatec.apifatec.controllers.exercicios;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
 
 @RestController // define ser o principal
 @SpringBootApplication
